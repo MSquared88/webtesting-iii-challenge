c312c07951773fd898b0bc2012ba1547c49bdc3d
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-16 17:22:34 -0500

    working on tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+
+
+// import React from 'react'
+// import { render, fireEvent } from 'react-testing-library'
+// import Controls from '../controls/Controls';
+
+// test("app loads with state of 0", () =>{
+//     const toggleLocked = jest.mock()
+
+//     const { getByTestId } = render(<Controls/>);
+
+//     fireEvent.click(doorLockStatus)
+    
+  
+//   })
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,7 +3,32 @@
 import React from 'react'
 import { render } from 'react-testing-library'
 import Dashboard from './Dashboard';
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
 
 test('should match snapshot', () => {
   expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+
+test('shows the controls and display', () => {
+    const toggleLocked = jest.fn();
+
+    render(
+      <>
+          <Display locked={'locked'} closed={'closed'} />
+          <Controls
+            toggleLocked={toggleLocked}
+            locked={'locked'}
+            closed={'closed'}
+          />
+      </>
+    );
+  });
+
+  test('defaults to unlock and open', () => {
+  const { getByText } = render(<Dashboard />);
+  getByText(/unlocked/i);
+  getByText(/open/i);
+
 });
